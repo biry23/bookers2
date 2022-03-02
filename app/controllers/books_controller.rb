1e09fd517c2a1be39ca61791e4242d81c03490a4
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-02 12:28:29 +0000

    [add] homes,books,users_controller作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def new
+    @book = @Books
+  end
+  
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  resources :books
   devise_for :users
   root to: "homes#top"
 
