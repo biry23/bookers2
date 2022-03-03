4e93366ee28f68728410c2359e7587fbd070fb75
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-03 11:12:10 +0000

    headerのレイアウト作成 link_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def create
+    @user = User.find(params[:id])
+
   end
 
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def index
+
   end
 
 
