dd49979a0391c395979820ff45a28d942ac58f06
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-12 08:16:27 +0000

    [Fix]画像にもリンクが必要なところにリンクをつける記述を追加,Fontawesomeの指定画像が違ったのを修正 cssとhtmlでのclassの記述の違いに注意

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       @user = current_user
       @books = Book.all
